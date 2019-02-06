a7dec1bb8c40d2eedea4fe44b6b3fa8bea03ca5e
Ferdy Budhidharma <ferd312@gmail.com>
2019-02-06 10:00:29 -0600

    remove unneccessary jest config

--- jest.config.js
+++ jest.config.js
@@ -4,12 +4,8 @@ function getMockFile(path) {
 
 module.exports = {
   modulePaths: ['src'],
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
   setupFiles: ['<rootDir>/jest.setup.js'],
   testURL: 'https://localhost',
-  transform: {
-    '\\.[tj]sx?$': 'babel-jest',
-  },
   moduleNameMapper: {
     '^astroturf$': getMockFile('astroturf'),
   },
