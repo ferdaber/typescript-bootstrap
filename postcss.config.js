e84be5eebe160f7d1bd9e402f2fd5a63915bf860
Ferdy Budhidharma <ferd312@gmail.com>
2019-08-30 15:13:59 -0500

    update packages, add Inter

--- postcss.config.js
+++ postcss.config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  plugins: [require('tailwindcss')('tailwind.js'), require('precss'), require('autoprefixer')],
+  plugins: [require('tailwindcss'), require('precss'), require('autoprefixer')],
   sourceMap: true,
 }
